6f2c127cc0061fec624c9e839ce16df9a4e3d2a7
carlpadilla <carlpadila@protonmail.com>
2019-08-21 22:04:45 -0400

    building out landing page

--- howto/src/components/LandingPage/LandingPage.js
+++ howto/src/components/LandingPage/LandingPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import logo from '../../assets/landing-img.png';
+import './landingpage.scss';
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>LANDING PAGE</h1>
+    <div className='landing-page-container'>
+      <img src={logo} alt='logo' />
+      <h1>I can do that!</h1>
+      <h3>
+        Life hacks for the optimistic in you, by people just like you.,
+        <br />
+        Have some know-how? Pay it forward.
+      </h3>
+      <button>Sign Up</button>
+      <button>Login</button>
+      <p>Copyright © 2019 ICDT, LLC No Rights Reserved</p>
     </div>
   );
 };
