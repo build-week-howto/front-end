231b1a62cee6173302b05ca0f7b1a720633f70c1
carlpadilla <carlpadila@protonmail.com>
2019-08-21 23:02:44 -0400

    added sign in and sign up components, along with Routes in  Appjs

--- howto/src/App.js
+++ howto/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 //components
 import LandingPage from './components/LandingPage/LandingPage';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/Signup/SignUp';
 
 function App() {
   return (
     <div className='App'>
-      <LandingPage />
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route path='/signup' component={SignUp} />
+        <Route path='/signin' component={SignIn} />
+      </Switch>
     </div>
   );
 }
