18ea4a3dd0bcd12fe3a61e7f03584d45e1c8eb46
carlpadilla <carlpadila@protonmail.com>
2019-08-29 15:05:31 -0400

    added content to search component

--- howto/src/components/SearchPage/SearchPage.js
+++ howto/src/components/SearchPage/SearchPage.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './searchpage.scss';
 
 const SearchPage = () => {
   return (
     <div>
-      <h1>Search Page</h1>
+      <section className='title'>
+        <div className='search'>
+          <label className='inputLabels' htmlFor='Search'>
+            <b>Search How To's</b>
+          </label>
+          <input
+            className='inputFields'
+            type='text'
+            placeholder='Search Here'
+            name='Search'
+          />
+        </div>
+      </section>
+      <section className='filter'>
+        <div className='filterTxt'>
+          <h2>Filter search results by:</h2>
+        </div>
+
+        <div className='buttons'>
+          <button>
+            <Link to='/home'>Keyword</Link>
+          </button>
+
+          <button>
+            <Link to='/profile'>Username</Link>
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
